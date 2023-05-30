24a4b3a2892e2b353b05bc1593feb8800aa41d1e
Lazar Dobrota <ldobrota7421rn@raf.rs>
2023-05-30 22:29:37 +0200

    Join je operator

---
+++ src/main/java/baze/model/implementation/operators/Join.java
@@ -0,0 +1,11 @@
+package baze.model.implementation.operators;
+
+public class Join extends Oprt{
+    public Join() {
+    }
+
+    @Override
+    public void doOperation(String[] line, int c) {
+        super.doOperation(line, c);
+    }
+}
